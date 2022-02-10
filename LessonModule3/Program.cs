4c1057e38359a4510cc9be3e667a39ed59c90889
SmokingRat <halfbrikbarry@gmail.com>
2022-02-10 14:02:48 +0300

    Добавьте файлы проекта.

---
+++ LessonModule3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LessonModule3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string MyName = "George";
+            Console.WriteLine(MyName);
+            Console.ReadLine();
+
+        }
+    }
+    
+}
