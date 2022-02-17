4f1ace538676e8ce559c66ea699b80b6f1c6c40c
SmokingRat <halfbrikbarry@gmail.com>
2022-02-17 17:51:44 +0300

    Модуль 3

--- LessonModule3/Program.cs
+++ LessonModule3/Program.cs
@@ -6,22 +6,45 @@ namespace LessonModule3
     {
         static void Main(string[] args)
         {
-            const string MyName = "George";
-            byte MyAge = 19;
-            bool MyPet = true;
-            float MySizeOfShoes = 37.5f;
+           
+         
 
-            Console.WriteLine(MyName);
-
-            Console.WriteLine("My name is " + MyName);
-            Console.WriteLine("I'm" + MyAge);
-            Console.WriteLine("Do you have a pet" + MyPet);
-            Console.WriteLine("My shoes size is " + MySizeOfShoes);
+            Console.WriteLine("What is your name? ");
+            var MyName = Console.ReadLine();
+            Console.WriteLine("How old are you? ");
+            var MyAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your name is {0} and your age is {1}", MyName, MyAge);
+            Console.WriteLine("Enter your birthday: ");
+            var DayOfBirth = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Your birthday is " + DayOfBirth);
 
             Console.ReadKey();
 
 
+
+
         }
+        enum Semaphore
+        {
+            Red = 100,
+            Yellow = 200,
+            Green = 300
+        }
+        
+        enum DaysOfWeek
+        {
+            Mondae = 1,
+            Tuesday = 2,
+            Wednesday = 3,
+            Thursday = 4,
+            Friday = 5,
+            Saturday = 6,
+            Sundae = 7
+
+        }
+
     }
+
+
     
 }
